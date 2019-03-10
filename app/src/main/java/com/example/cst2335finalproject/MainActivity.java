9a19db67cd40e10f40300a25b97421d60d423b20
albert <albertworkaccount@hotmail.com>
2019-03-10 13:48:33 -0400

    Added toolbar to the main to access eachother's activities

--- app/src/main/java/com/example/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.cst2335finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
     Button  menubutton3;
     Button  menubutton4;
 
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
         menubutton1 = findViewById(R.id.menuButton1);
         menubutton2 = findViewById(R.id.menuButton2);
         menubutton3 = findViewById(R.id.menuButton3);
@@ -40,5 +52,52 @@ public class MainActivity extends AppCompatActivity {
             Intent nextPage = new Intent(MainActivity.this, NYTActivity.class);
             startActivity(nextPage);
         });
+
+
     }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu_nav, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent nextPage;
+
+        switch(item.getItemId()){
+
+            case R.id.dictionaryActivityMenuItem:
+                nextPage = new Intent(MainActivity.this, DictionaryActivity.class);
+                startActivity(nextPage);
+                break;
+
+            case R.id.newsfeedActivityMenuItem:
+                nextPage = new Intent(MainActivity.this, NewsfeedActivity.class);
+                startActivity(nextPage);
+                break;
+
+            case R.id.flightStatusTrackerMenuItem:
+                nextPage = new Intent(MainActivity.this, FlightTrackerActivity.class);
+                startActivity(nextPage);
+                break;
+
+
+                case R.id.newYorkTimesMenuItem:
+                    nextPage = new Intent(MainActivity.this, NYTActivity.class);
+                    startActivity(nextPage);
+                break;
+
+        }
+        return true;
+    }
+
+
+
 }
