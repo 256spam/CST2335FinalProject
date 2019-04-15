d825f5502dc70b274e199b6e8e69a9cdc371d9e7
Peter Best <256spam@Gmail.com>
2019-04-15 14:17:34 -0400

    fix dictionary fragment

--- app/src/main/java/com/example/cst2335finalproject/DictionaryActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/DictionaryActivity.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
