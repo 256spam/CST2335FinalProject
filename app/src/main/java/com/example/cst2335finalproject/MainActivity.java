f7bab2b3418fa7776f059081a7d06562c471959f
Peter Best <256spam@gmail.com>
2019-04-18 13:32:39 -0400

    Reverting joshua's late additions after code freeze

--- app/src/main/java/com/example/cst2335finalproject/MainActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/MainActivity.java
@@ -1,133 +1,77 @@
-package com.example.cst2335finalproject.NYTArticle;
+package com.example.cst2335finalproject;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-
-import com.example.cst2335finalproject.dictionaryActivity.DictionaryActivity;
-import com.example.cst2335finalproject.flightstatusActivity.FlightActivity;
-import com.example.cst2335finalproject.NYTArticle.MainActivityNewYorkTimes;
-import com.example.cst2335finalproject.newsfeedActivity.NewsFeedActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar tBar;
+    Toolbar toolbar;
 
+    /**
+     * When program starts and activity is created, establish toolbar and set it
+     * @param savedInstanceState bundle of info from last saved instance
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.example.cst2335finalproject.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        tBar = (Toolbar)findViewById(com.example.cst2335finalproject.R.id.toolbar);
-        setSupportActionBar(tBar);
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
-        Button dicBtn = findViewById(com.example.cst2335finalproject.R.id.enter_dic);
-        Button fstBtn = findViewById(com.example.cst2335finalproject.R.id.enter_fst);
-        Button nfBtn = findViewById(com.example.cst2335finalproject.R.id.enter_nf);
-        Button nytBtn = findViewById(com.example.cst2335finalproject.R.id.enter_nyt);
-       // ImageButton nytMenuBtn = findViewById(R.id.nyt_menu_KN);
-
-
-        // when user click on enter dictionary button
-        dicBtn.setOnClickListener(c->{
-            Intent nextPage = new Intent(MainActivity.this, DictionaryActivity.class);
-            startActivity(nextPage);
-        });
-        // when user click on enter flight status tracker button
-        fstBtn.setOnClickListener(c->{
-            Intent nextPage = new Intent(MainActivity.this, FlightActivity.class);
-            startActivity(nextPage);
-        });
-        // when user click on enter news feed button
-        nfBtn.setOnClickListener(c->{
-            Intent nextPage = new Intent(MainActivity.this, NewsFeedActivity.class);
-            startActivity(nextPage);
-        });
-        // when user click on enter new york times article button
-        nytBtn.setOnClickListener(c->{
-            Intent nextPage = new Intent(MainActivity.this, MainActivityNewYorkTimes.class);
-            startActivity(nextPage);
-        });
-       // nytMenuBtn.setOnClickListener(c->{
-//            Intent nextPage = new Intent(MainActivity.this, MainActivityNewYorkTimes.class);
-//            startActivity(nextPage);
-//        });
     }
 
+
     /**
-     * inflate the icons for toolbar
-     * @param menu
+     *  inflates given menu to create options menu
+     * @param menu menu to inflate
      * @return
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(com.example.cst2335finalproject.R.menu.toolbar_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu_nav, menu);
         return true;
     }
 
     /**
-     * when click on the icons
-     * @param item
-     * @return
+     * when one of the options in the toolbar is selected, move to related activity
+     * @param item the item that was clicked
+     * @return that it was selected
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Intent nextPage = null;
-        switch(item.getItemId())
-        {
-            //when click on "dictionary":
-            case R.id.go_dic:
+
+        Intent nextPage;
+
+        switch(item.getItemId()){
+
+            case R.id.dictionaryActivityMenuItem:
                 nextPage = new Intent(MainActivity.this, DictionaryActivity.class);
                 startActivity(nextPage);
                 break;
-            // when click on "flight tracker":
-            case R.id.go_flight:
-                nextPage = new Intent(MainActivity.this, FlightActivity.class);
+
+            case R.id.newsfeedActivityMenuItem:
+                nextPage = new Intent(MainActivity.this, NewsfeedActivity.class);
                 startActivity(nextPage);
                 break;
-            // when click on "news feed":
-            case R.id.go_news_feed:
-                nextPage = new Intent(MainActivity.this, NewsFeedActivity.class);
+
+            case R.id.flightStatusTrackerMenuItem:
+                nextPage = new Intent(MainActivity.this, FlightTrackerActivity.class);
                 startActivity(nextPage);
                 break;
-            // when click on "new york times":
-            case R.id.go_new_york:
-                nextPage = new Intent(MainActivity.this, MainActivityNewYorkTimes.class);
+
+            case R.id.newYorkTimesMenuItem:
+                nextPage = new Intent(MainActivity.this, NYTActivity.class);
                 startActivity(nextPage);
                 break;
         }
         return true;
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 }
